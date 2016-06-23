81388f78f4e38601ffffc08d9b4c79653cac671c
Julia Schultz <julia.schultz91@gmail.com>
2016-06-23 17:57:20 -0400

    updates to toggle divs instead of change pages

--- js.js
+++ js.js
@@ -1,7 +1,25 @@
+
+$('#mywork').addClass('hidden')
+$('#aboutMe').addClass('hidden')
+
+
 $(window).scroll(function() {
-				if ($(window).scrollTop() > 315) {
-					$('.nav').addClass('stuck');
-				} else {
-					$('.nav').removeClass('stuck');
-				}
-			});
+  if ($(window).scrollTop() > 315) {
+    $('.nav').addClass('stuck');
+  } else {
+    $('.nav').removeClass('stuck');
+  }
+});
+
+$('#work').on('click', function(){
+  // $('.subpage').toggleClass('hidden')
+  $('#mywork').removeClass('hidden')
+  $('#aboutMe').addClass('hidden')
+  $('#blockOne').addClass('hidden')
+})
+
+$('#about').on('click', function(){
+  $('#aboutMe').removeClass('hidden')
+  $('#mywork').addClass('hidden')
+  $('#blockOne').addClass('hidden')
+})
