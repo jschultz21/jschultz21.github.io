b598101bc5fc6e6e97a00f925d907f62c75abc46
Julia Schultz <julia.schultz91@gmail.com>
2016-08-02 09:52:46 -0400

    small changes

--- js.js
+++ js.js
@@ -14,42 +14,18 @@ $(window).scroll(function() {
 $('#work').on('click', function(){
   $('#mywork').removeClass('hidden')
   $('#aboutMe').addClass('hidden')
-  $('#home').addClass('hidden')
+  $('#homepage').addClass('hidden')
 })
 
 $('#about').on('click', function(){
-//   var stateObj = $('#about')
-//   var currentState = history.state;
-// console.log(stateObj)
-// console.log(currentState)
-var stateObj = { foo: "bar" };
-history.pushState(stateObj, "page 2", "about.html");
-
-
   $('#aboutMe').removeClass('hidden')
   $('#mywork').addClass('hidden')
-  $('#home').addClass('hidden')
+  $('#homepage').addClass('hidden')
 })
 
 
-// function getHomePage(){
-//   home = $('#home');
-// }
-// history.pushState([data], [title], [url]);
-//
-//
-// var router = new staterouter.Router();
-// router.route('/', getHomePage).route('/aboutMe', getAboutMe).route('/mywork', mywork);
-// router.navigate('/');
-//
-// function loadNext(url) {
-//     $('#body').queue(function(next) {
-//         setTimeout(function() {
-//             router.navigate(url);
-//             next();
-//         }, 3000);
-//     });
-// }
-//
-// loadNext('/aboutMe');
-// loadNext('/mywork');
+$('#home').on('click', function(){
+  $('#homepage').removeClass('hidden')
+  $('#mywork').addClass('hidden')
+  $('#aboutMe').addClass('hidden')
+})
